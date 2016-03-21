e8736a6310e79973dd0e1254aaf25be088d5af96
synouk <crousseau@intechinfo.fr>
2016-03-21 15:53:35 +0100

    Création

---
+++ Cake/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake
+{
+    public class Class1
+    {
+    public int add (int a, int b)
+        {
+            return (a + b);
+        }
+
+        public int less(int a, int b)
+        {
+            return (a - b);
+        }
+    }
+
+    
+}
---
+++ TestCake/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Cake;
+
+namespace cake.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void additionTest()
+        {
+            Class1 cake = new Class1();
+            Assert.AreEqual(2, cake.add(1, 1));
+        }
+
+        [Test]
+        public void soustractionTest()
+        {
+            Class1 cake = new Class1();
+            Assert.AreEqual(2, cake.less(3, 1));
+        }
+    }
+}
