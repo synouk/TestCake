98bbe42a7a166ff9317ec4e77fd7b7b234a848ec
synouk <crousseau@intechinfo.fr>
2016-03-21 16:54:03 +0100

    modification yml

--- CodeCakeBuilder1/Build.cs
+++ CodeCakeBuilder1/Build.cs
@@ -118,13 +118,13 @@ namespace CodeCake
                     // AND CodeCakeBuilder1 is running in interactive mode (ie. no -nointeraction parameter),
                     // then the user is prompted to enter it.
                     // This is specific to CodeCake (in Code.Cake.dll).
-                    var apiKey = Cake.InteractiveEnvironmentVariable("NUGET_API_KEY");
-                   if (string.IsNullOrEmpty(apiKey)) throw new InvalidOperationException("Could not resolve Myget API key.");
+//                    var apiKey = Cake.InteractiveEnvironmentVariable("NUGET_API_KEY");
+  //                 if (string.IsNullOrEmpty(apiKey)) throw new InvalidOperationException("Could not resolve Myget API key.");
 
                    var settings = new NuGetPushSettings
                    {
                        Source = "https://www.myget.org/feed/Packages/test-cake",
-                       ApiKey = apiKey
+    //                   ApiKey = apiKey
                    };
 
                    foreach (var nupkg in Cake.GetFiles(releasesDir.Path + "/*.nupkg"))
